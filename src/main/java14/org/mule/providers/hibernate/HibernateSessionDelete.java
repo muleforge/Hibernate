7fa88c71bdd24266917c6fd64e8231c8866d5a41
mverbert <mverbert@244999f3-a746-0410-a9f5-ae86ec22bd0b>
2008-04-25 17:40:04 +0000

    split 1.X from 2.0, code is way to different (and more to come)

--- src/main/java14/org/mule/providers/hibernate/HibernateSessionDelete.java
+++
@@ -1,8 +0,0 @@
-package org.mule.providers.hibernate;
-
-import org.hibernate.Session;
-
-public interface HibernateSessionDelete {
-
-	public void delete(Session session, Object payload);
-}
--- src/main/java14/org/mule/providers/hibernate/HibernateSessionMerge.java
+++
@@ -1,9 +0,0 @@
-package org.mule.providers.hibernate;
-
-import org.hibernate.Session;
-
-public interface HibernateSessionMerge {
-
-	public Object merge(Session session, Object payload);
-	
-}
